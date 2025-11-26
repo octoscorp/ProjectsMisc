0f05055402121a84fd9c097e6105146c5a4ab677
octoscorp <gfthampton@gmail.com>
2025-11-26 15:11:21 +1300

    BotC: Add search running wrapper
    Also implement limited-space exhaustion search by removing non-jinxed
    characters.

--- BotC/max-jinx-script.py
+++ BotC/max-jinx-script.py
@@ -63,6 +63,10 @@ class _JinxGraph():
             for jinxed_char_name in jinx_dict[char_name].keys():
                 dict_member_append(self.adj_list, char_name, jinxed_char_name)
 
+    def is_jinxed_char(self, char_name):
+        """Return bool of whether a jinx exists for char_name"""
+        return self.adj_list.get(char_name) is not None
+
     def get_num_jinxes(self, char_list):
         """
         Returns number of jinxes obtained by having the character set in play.
@@ -88,58 +92,111 @@ class _JinxGraph():
         return count
 
 
-def exhaustion_search(graph):
-    """
-    Cleverer search solutions exist, however I want to be sure I haven't missed anything
-    """
-    global checked_solns, last_soln
+class Search:
+    EXHAUSTION = 1
+    EXHAUSTION_REDUCED = 2
 
-    max_jinxes = 0
-    optimal_solutions = []
-    # Try every combination!
-    for townsfolk in combinations(characters["townsfolk"].keys(),
-                                       TOWN_DISTRIBUTION["townsfolk"]):
-        for outsiders in combinations(characters["outsiders"].keys(),
-                                           TOWN_DISTRIBUTION["outsiders"]):
-            for minions in combinations(characters["minions"].keys(),
-                                            TOWN_DISTRIBUTION["minions"]):
-                for demons in combinations(characters["demons"].keys(),
-                                                TOWN_DISTRIBUTION["demons"]):
-                    chars = list(townsfolk + outsiders + minions + demons)
-                    # The magic check
-                    num_jinxes = graph.get_num_jinxes(chars)
-                    if num_jinxes > max_jinxes:
-                        max_jinxes = num_jinxes
-                        optimal_solutions = []
-                    if num_jinxes == max_jinxes:
-                        optimal_solutions.append(chars)
+    def __init__(self, search_type=EXHAUSTION):
+        """Search type: One of Search.* enum, or a callable search func"""
+        self.search_type = search_type
 
-                    # Update for SIGINFO
-                    checked_solns += 1
-                    last_soln = chars
-    return optimal_solutions
+    def output_scripts(self, solutions):
+        """
+        TODO: Write to script file
+        """
+        print(solutions)
 
+    def run(self):
+        global start_time
+        graph = _JinxGraph(jinxes)
 
-def output_scripts(solutions):
-    """
-    TODO: Write to script file
-    """
-    print(solutions)
+        signal.signal(signal.SIGINT, sigint_handler)
+        start_time = datetime.now()
+
+        # Determine which search to run
+        search_func = None
+        match self.search_type:
+            case Search.EXHAUSTION_REDUCED:
+                search_func = self.reduced_space_exhaustion_search
+            case Search.EXHAUSTION:
+                search_func = self.exhaustion_search
+            case _:
+                search_func = self.search_type
+
+        solutions = search_func(graph)
+
+        elapsed_time = datetime.now() - start_time
+        print(f"Took {elapsed_time} seconds to complete")
+
+        self.output_scripts(solutions)
+
+    def exhaustion_search(self, graph):
+        """
+        Cleverer search solutions exist, however I want to be sure I haven't missed anything
+        """
+        global checked_solns, last_soln
+
+        max_jinxes = 0
+        optimal_solutions = []
+        # Try every combination!
+        for townsfolk in combinations(characters["townsfolk"].keys(),
+                                        TOWN_DISTRIBUTION["townsfolk"]):
+            for outsiders in combinations(characters["outsiders"].keys(),
+                                            TOWN_DISTRIBUTION["outsiders"]):
+                for minions in combinations(characters["minions"].keys(),
+                                                TOWN_DISTRIBUTION["minions"]):
+                    for demons in combinations(characters["demons"].keys(),
+                                                    TOWN_DISTRIBUTION["demons"]):
+                        chars = list(townsfolk + outsiders + minions + demons)
+                        # The magic check
+                        num_jinxes = graph.get_num_jinxes(chars)
+                        if num_jinxes > max_jinxes:
+                            max_jinxes = num_jinxes
+                            optimal_solutions = []
+                        if num_jinxes == max_jinxes:
+                            optimal_solutions.append(chars)
+
+                        # Update for SIGINFO
+                        checked_solns += 1
+                        last_soln = chars
+        return optimal_solutions
+
+    def reduced_space_exhaustion_search(self, graph):
+        """Exhaustion search with non-jinxed characters removed"""
+        global checked_solns, last_soln
+
+        max_jinxes = 0
+        optimal_solutions = []
+
+        # Remove non-jinxed characters
+        town = [char for char in characters["townfolk"].keys() if graph.is_jinxed_char(char)]
+        outs = [char for char in characters["outsiders"].keys() if graph.is_jinxed_char(char)]
+        mins = [char for char in characters["minions"].keys() if graph.is_jinxed_char(char)]
+        dems = [char for char in characters["demons"].keys() if graph.is_jinxed_char(char)]
+
+        # Try every remaining combination
+        for townsfolk in combinations(town, TOWN_DISTRIBUTION["townsfolk"]):
+            for outsiders in combinations(outs, TOWN_DISTRIBUTION["outsiders"]):
+                for minions in combinations(mins, TOWN_DISTRIBUTION["minions"]):
+                    for demons in combinations(dems, TOWN_DISTRIBUTION["demons"]):
+                        chars = list(townsfolk + outsiders + minions + demons)
+                        # The magic check
+                        num_jinxes = graph.get_num_jinxes(chars)
+                        if num_jinxes > max_jinxes:
+                            max_jinxes = num_jinxes
+                            optimal_solutions = []
+                        if num_jinxes == max_jinxes:
+                            optimal_solutions.append(chars)
+
+                        # Update for SIGINFO
+                        checked_solns += 1
+                        last_soln = chars
+        return optimal_solutions
 
 
 def main():
-    global start_time
-    graph = _JinxGraph(jinxes)
-
-    signal.signal(signal.SIGINT, sigint_handler)
-    start_time = datetime.now()
-
-    solutions = exhaustion_search(graph)
-
-    elapsed_time = datetime.now() - start_time
-    print(f"Took {elapsed_time} seconds to complete")
-
-    output_scripts(solutions)
+    search = Search(Search.EXHAUSTION_REDUCED)
+    search.run()
 
 
 if __name__ == "__main__":
